1075187024f1a232ffd85a59cb5b62ea34837123
MARIA-DIANA MOLDOVAN <maria.diana.moldovan@stud.ubbcluj.ro>
2021-01-09 13:29:29 +0200

    Add project files.

---
+++ Data/Moldovan_Maria_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Maria_Lab6.Models;
+
+namespace Moldovan_Maria_Lab6.Data
+{
+    public class Moldovan_Maria_Lab6Context : DbContext
+    {
+        public Moldovan_Maria_Lab6Context (DbContextOptions<Moldovan_Maria_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Maria_Lab6.Models.Book> Book { get; set; }
+    }
+}
